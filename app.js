9508a292bda10f5191b380f27646b3cfe4433216
Styoks <113370103+Styoks@users.noreply.github.com>
2024-05-26 21:04:29 +0000

    finalTest

---
+++ app.js
@@ -0,0 +1,15 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+const fromDollarToYen  = function(valueInDollar) {
+    let valueInYen = valueInDollar * 1.07 * 156.5;
+    return valueInYen;
+}
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 1.07 * 156.5 * 0.87;
+    return valueInPound;
+}
+module.exports = { fromEuroToDollar }
